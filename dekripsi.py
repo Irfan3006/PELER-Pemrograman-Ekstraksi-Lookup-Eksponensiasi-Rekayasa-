5b6b768d82b2dcefe98091585ac9063a46c402ce
Irfan Syarifudin <irfansyarifudin79@gmail.com>
2025-12-24 17:19:07 +0700

    Initial commit

---
+++ dekripsi.py
@@ -0,0 +1,62 @@
+# dekripsi
+mapping = {
+    "A": "da778688000ji",
+    "B": "ic7575729344jktk",
+    "C": "c7668682048dr",
+    "D": "hi7940149875ah",
+    "E": "ng7833173256ja",
+    "F": "an7495014493jb",
+    "G": "go8108486729rgrpkt",
+    "H": "ll7880599000spj",
+    "I": "io7988005999bf",
+    "J": "va7940149875jg",
+    "K": "in8132727331ab",
+    "L": "sp7506509912jm",
+    "M": "ab7809531904cm",
+    "N": "im8096384512ar",
+    "O": "ml7952095936xl",
+    "P": "on7868724669gvr",
+    "Q": "rp20178205738913mr",
+    "R": "by7940149875ym",
+    "S": "ft8169178744cl",
+    "T": "pt8144865728ah",
+    "U": "da7988005999dm",
+    "V": "v8230172859am",
+    "W": "ly8181353375wc",
+    "X": "ry8036054027mk",
+    "Y": "ck7952095936dhm",
+    "Z": "ig8193540096ak",
+    "0": "ar8120601000mh",
+    "1": "ct8157016197jw",
+    "2": "ue8169178744ey",
+    "3": "te8193540096rh",
+    "4": "er8132727331yk",
+    "5": "ne8120601000ja",
+    "6": "ry8072216216jr",
+    "7": "ap8132727331mojt",
+    "8": "xt8193540096gr",
+    "9": "id8217949832rc"
+}
+
+reverse_mapping = {v: k for k, v in mapping.items()}
+
+def decrypt(ciphertext):
+    plaintext = ""
+    i = 0
+    while i < len(ciphertext):
+        match = False
+        for code, letter in reverse_mapping.items():
+            if ciphertext.startswith(code, i):
+                plaintext += letter
+                i += len(code)
+                match = True
+                break
+        if not match:
+            i += 1
+            return "Tidak ditemukan"
+    return plaintext
+
+if __name__ == "__main__":
+    ct = input("Masukkan ciphertext: ")
+    hasil = decrypt(ct)
+    print("Plaintext:", hasil)
---
+++ enkripsi.py
@@ -0,0 +1,52 @@
+# enkripsi
+mapping = {
+    "A": "da778688000ji",
+    "B": "ic7575729344jktk",
+    "C": "c7668682048dr",
+    "D": "hi7940149875ah",
+    "E": "ng7833173256ja",
+    "F": "an7495014493jb",
+    "G": "go8108486729rgrpkt",
+    "H": "ll7880599000spj",
+    "I": "io7988005999bf",
+    "J": "va7940149875jg",
+    "K": "in8132727331ab",
+    "L": "sp7506509912jm",
+    "M": "ab7809531904cm",
+    "N": "im8096384512ar",
+    "O": "ml7952095936xl",
+    "P": "on7868724669gvr",
+    "Q": "rp20178205738913mr",
+    "R": "by7940149875ym",
+    "S": "ft8169178744cl",
+    "T": "pt8144865728ah",
+    "U": "da7988005999dm",
+    "V": "v8230172859am",
+    "W": "ly8181353375wc",
+    "X": "ry8036054027mk",
+    "Y": "ck7952095936dhm",
+    "Z": "ig8193540096ak",
+    "0": "ar8120601000mh",
+    "1": "ct8157016197jw",
+    "2": "ue8169178744ey",
+    "3": "te8193540096rh",
+    "4": "er8132727331yk",
+    "5": "ne8120601000ja",
+    "6": "ry8072216216jr",
+    "7": "ap8132727331mojt",
+    "8": "xt8193540096gr",
+    "9": "id8217949832rc",
+}
+
+def encrypt(text):
+    text = text.upper()
+    ciphertext = ""
+    for ch in text:
+        if ch in mapping:
+            ciphertext += mapping[ch]
+    return ciphertext
+
+if __name__ == "__main__":
+    plaintext = input("Masukkan teks: ")
+    hasil = encrypt(plaintext)
+    print("Ciphertext:", hasil)
